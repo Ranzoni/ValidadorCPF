7e7ae9101288cea67b6be3f650f039ead5d1eb4a
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2024-01-04 18:30:13 -0300

    Subindo o projeto

---
+++ ValidacaoCPF/Cpf.cs
@@ -0,0 +1,63 @@
+﻿namespace ValidacaoCPF
+{
+    public static class Cpf
+    {
+        private const int QTD_CARACTERES_DIG_CPF = 2;
+
+        public static bool Validar(string? cpf)
+        {
+            if (string.IsNullOrEmpty(cpf) || cpf.Length < 11)
+                return false;
+
+            return DigitoEhValido(cpf);
+        }
+
+        private static bool DigitoEhValido(string cpf)
+        {
+            return PrimeiroDigitoEhValido(cpf) && SegundoDigitoEhValido(cpf);
+        }
+
+        private static bool PrimeiroDigitoEhValido(string cpf)
+        {
+            var somaNumerosCpf = 0;
+            var contador = 10;
+            for (var i = 0 ; i <= cpf.Length - (QTD_CARACTERES_DIG_CPF + 1); i++)
+            {
+                somaNumerosCpf += int.Parse(cpf[i].ToString()) * contador;
+                contador--;
+            }
+
+            var restoDivisao = CalcularRestoDivisao(somaNumerosCpf, cpf.Length);
+            var digitosCpf = RetornarDigitoCpf(cpf);
+
+            return int.Parse(digitosCpf[0].ToString()) == restoDivisao;
+        }
+
+        private static bool SegundoDigitoEhValido(string cpf)
+        {
+            var somaNumerosCpf = 0;
+            var contador = 11;
+            for (var i = 0; i <= cpf.Length - QTD_CARACTERES_DIG_CPF; i++)
+            {
+                somaNumerosCpf += int.Parse(cpf[i].ToString()) * contador;
+                contador--;
+            }
+
+            var restoDivisao = CalcularRestoDivisao(somaNumerosCpf, cpf.Length);
+            var digitosCpf = RetornarDigitoCpf(cpf);
+
+            return int.Parse(digitosCpf[1].ToString()) == restoDivisao;
+        }
+
+        private static string RetornarDigitoCpf(string cpf)
+        {
+            return cpf.Substring(cpf.Length - QTD_CARACTERES_DIG_CPF, QTD_CARACTERES_DIG_CPF);
+        }
+
+        private static int CalcularRestoDivisao(int num1, int num2)
+        {
+            var restoDivisao = num1 * 10 % num2;
+            return restoDivisao == 10 ? 0 : restoDivisao;
+        }
+    }
+}
---
+++ ValidacaoCPF/Program.cs
@@ -0,0 +1,24 @@
+﻿using ValidacaoCPF;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Informe o CPF COMPLETO:");
+    var cpf = Console.ReadLine();
+
+    Console.Clear();
+
+    if (Cpf.Validar(cpf))
+        Console.WriteLine("CPF válido");
+    else
+        Console.WriteLine("CPF inválido");
+
+    Console.WriteLine();
+    Console.WriteLine("Deseja continuar?");
+    Console.WriteLine("1 - Sim");
+    Console.WriteLine("2 - Não");
+    var opcao = int.Parse(Console.ReadLine() ?? "");
+    if (opcao != 1)
+        break;
+
+} while (true);
